4db2629d60e17a6ca8519c32644cbeb2c70e419a
sutigit <panyanilben@gmail.com>
2022-12-10 12:03:43 +0200

    modified gitignore

--- config/config.py
+++
@@ -1,6 +0,0 @@
-# Choose your main editor to open the diary pages
-# example "nano" for nano editor or "code" for vscode
-EDITOR = None
-
-# Choose your directory to where to create diary templates
-PATH = None
